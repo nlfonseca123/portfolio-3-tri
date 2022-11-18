3d6c70015b5ad3e740594e50b088a1cde4472b5d
nlfonseca123 <102531987+nlfonseca123@users.noreply.github.com>
2022-11-18 08:41:33 -0300

    Create informação ao usuario.sh

---
+++ informação ao usuario.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Bom dia! Qual é o seu nome?" #Printa no terminal o texto entre aspas.
+read nome #Lê o que for escrito pelo usuário e o armazena na variável "nome".
+echo "" #Printa no terminal o texto entre aspas.
+echo "Olá, $nome" #Printa no terminal o texto entre aspas junto da variável "nome".
+
+#Quando o usuário fornece a informação para o script, as variáves podem ser definidas na hora, sem uma pré definição padrão das mesmas.
